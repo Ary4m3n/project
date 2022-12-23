057229e5a80e4e5d0da9c850823bbfc3f6401194
Aryaman Sharma <98371388+Ary4m3n@users.noreply.github.com>
2022-12-23 00:43:02 -0500

    Update SignUpView.java

--- src/FrameworksDrivers/SignUpView.java
+++ src/FrameworksDrivers/SignUpView.java
@@ -1,4 +1,15 @@
 package FrameworksDrivers;
 
 public class SignUpView {
+  // add all the UI elements to make the Signup view first then move on to the presenter. 
+  // when you are coding the presenter you will get clarification on how the Use Cases are going to turn out as. 
+  // keeo adding things to the entities as you go along coding the rest of the parts.
+  // you will have to make design decisons on how to arrange things in the UserMap class in the Entites Package.
+  
+  
+  // Moving on you will have to see that the project is actually going to be:
+  // let see, mmm what all ideas do we have:
+  // - stock tracking
+  // - 
+
 }
